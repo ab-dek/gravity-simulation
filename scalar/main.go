b20bc7689d9be0b5038d6add9bf1f69fbdd1b479
abenidek <abenezerd422@gmail.com>
2026-09-04 22:44:43 +0300

    big bang

---
+++ scalar/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
+
+func main() {
+	rl.InitWindow(800, 450, "Gravity Simulation")
+	defer rl.CloseWindow()
+
+	rl.SetTargetFPS(60)
+
+	for !rl.WindowShouldClose() {
+		rl.BeginDrawing()
+		rl.ClearBackground(rl.Black)
+
+		rl.EndDrawing()
+	}
+}
